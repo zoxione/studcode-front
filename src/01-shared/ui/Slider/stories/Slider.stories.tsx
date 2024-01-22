1924b36039fd49a86e9813d907cdc12c5757ffea
Bair <73276298+zoxione@users.noreply.github.com>
2024-01-22 19:07:31 +0300

    add Slider

---
+++ src/01-shared/ui/Slider/stories/Slider.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { Slider } from "../ui/Slider"
+
+const meta = {
+  title: "UI/Slider",
+  component: Slider,
+  parameters: {},
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof Slider>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    defaultValue: 50,
+    max: 100,
+    step: 1,
+  },
+}
