c25db6c515a57ee81b66b17334fb574977a75017
Bair <73276298+zoxione@users.noreply.github.com>
2024-06-05 15:23:13 +0300

    updated auth form

--- src/04-widgets/auth-dialog/ui/auth-dialog.tsx
+++ src/04-widgets/auth-dialog/ui/auth-dialog.tsx
@@ -4,8 +4,7 @@ import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
-import { Button } from "@/01-shared/ui/button"
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/01-shared/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/01-shared/ui/card"
 import { Dialog, DialogContent } from "@/01-shared/ui/dialog"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/01-shared/ui/tabs"
 import { SignInForm, SignUpForm } from "@/03-features/auth"
@@ -44,14 +43,21 @@ const AuthDialog = () => {
               <CardHeader>
                 <CardTitle className="text-center">Авторизация</CardTitle>
               </CardHeader>
-              <CardContent>
+              <CardContent className="space-y-4">
                 <SignInForm />
+                <p className="text-center text-sm text-muted-foreground">
+                  Входя в систему или регистрируясь в ней, вы принимаете нашу{" "}
+                  <Link href="/privacy" target="_blank" className="underline underline-offset-4 hover:text-primary">
+                    Политику конфиденциальности
+                  </Link>
+                  .
+                </p>
               </CardContent>
-              <CardFooter className="justify-center text-center">
+              {/* <CardFooter className="justify-center text-center">
                 <Button variant="link" size="none" asChild>
                   <Link href={`/`}>Забыли пароль?</Link>
                 </Button>
-              </CardFooter>
+              </CardFooter> */}
             </Card>
           </TabsContent>
           <TabsContent value="sign-up">
@@ -59,8 +65,15 @@ const AuthDialog = () => {
               <CardHeader>
                 <CardTitle className="text-center">Регистрация</CardTitle>
               </CardHeader>
-              <CardContent>
+              <CardContent className="space-y-4">
                 <SignUpForm />
+                <p className="text-center text-sm text-muted-foreground">
+                  Входя в систему или регистрируясь в ней, вы принимаете нашу{" "}
+                  <Link href="/privacy" target="_blank" className="underline underline-offset-4 hover:text-primary">
+                    Политику конфиденциальности
+                  </Link>
+                  .
+                </p>
               </CardContent>
             </Card>
           </TabsContent>
